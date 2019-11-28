56c06bf0828ea6abb0b44149b83268985eab6a67
culjo <oladosulekan@yahoo.com>
2019-11-28 13:41:02 +0100

    Renamed the Validator methods with a is prefix added more validators

--- lib/the_validator.dart
+++ lib/the_validator.dart
@@ -3,12 +3,11 @@ library the_validator;
 //import 'package:flutter/widgets.dart' show FormFieldValidator;
 import 'package:flutter/widgets.dart';
 
-/**
- * Todo:
- * - Collect list of TextField controllers
- * - Use
- * - Specify our error label using dart streams
- */
+
+/// - Collect list of TextField controllers
+/// - Specify our error label using dart streams
+///
+///
 class ErrorLabel extends StatelessWidget {
   ValueNotifier<String> text = new ValueNotifier("");
 
@@ -29,7 +28,12 @@ class ErrorLabel extends StatelessWidget {
 
 /// A Validator.
 class Validator {
-  static bool required({String value, bool allowEmptySpaces = true}) {
+
+  /*static bool notNull(String value) {
+    return value != null;
+  }*/
+
+  static bool isRequired({String value, bool allowEmptySpaces = true}) {
     if (value == null || value.isEmpty) {
       return false;
     } else {
@@ -42,8 +46,8 @@ class Validator {
     }
   }
 
-  static bool email(String email) {
-    if (!required(value: email)) return false;
+  static bool isEmail(String email) {
+    if (!isRequired(value: email)) return false;
 
     final emailRegex = RegExp(
         r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
@@ -54,7 +58,7 @@ class Validator {
   }
 
   /// Todo: Implement reason for failure
-  static bool password(String password,
+  static bool isPassword(String password,
       {int minLength = 4,
       int maxLength,
       bool shouldContainNumber = false,
@@ -89,12 +93,23 @@ class Validator {
 
   }
 
-  static bool equalTo(dynamic value, dynamic valueToCompare) {
+  static bool isEqualTo(dynamic value, dynamic valueToCompare) {
     if (value == valueToCompare) return true;
     else return false;
   }
 
-  static isNumber(String value) {}
+
+  static isNumber(String value, {bool noSymbols = true}) {
+    if (value == null) return false;
+
+    var numericRegEx = RegExp(r"^[+-]?([0-9]*[.])?[0-9]+$");
+    var numericNoSymbolsRegExp = RegExp(r"^[0-9]+$");
+
+    if (noSymbols) {
+      return numericNoSymbolsRegExp.hasMatch(value);
+    } else return numericRegEx.hasMatch(value);
+
+  }
 
   static bool minLength(String value, int minLength) {
     if (value.isEmpty) return false;
@@ -108,23 +123,82 @@ class Validator {
     else return false;
   }
 
+  static bool maxValue(double value, double maxValue) {
+    if (value < maxValue) return true;
+    return false;
+  }
+
+  static bool minValue(double value, double minValue) {
+    if (value > minValue) return true;
+    return false;
+  }
+
+  static bool isUppercase(String value) {
+    if (value == null) return false;
+    return value.compareTo(value.toUpperCase()) == 0;
+  }
+
+  static bool isLowercase(String value) {
+    if (value == null) return false;
+    return value.compareTo(value.toLowerCase()) == 0;
+  }
+
+  static bool isAlphaNumeric(String value) {
+    if (value == null) return false;
+    var alphaNumRegExp = RegExp(r"[0-9A-Z]+$");
+    return alphaNumRegExp.hasMatch(value);
+  }
+
+
+  // Todo:
+  static bool _isUrl(String url) {
+    return false;
+  }
+
+  // Todo: Add Date Validations also
+  _isDate() {
+
+  }
+
+  _isDateGreater() {
+
+  }
+
+
 }
 
-class FieldValidators {
+class FieldValidator {
   /// You can override the error message
-  static FormFieldValidator<String> required([String errorMessage]) {
+  static FormFieldValidator<String> required({String message}) {
     return (fieldValue) {
-      if (Validator.required(value: fieldValue)) {
+      if (Validator.isRequired(value: fieldValue)) {
         return null;
       } else {
-        return errorMessage ?? "This Field is Required";
+        return message ?? "This Field is Required";
       }
     };
   }
 
-  static FormFieldValidator<String> email([String errorMessage]) {
+  static FormFieldValidator<String> email({String message}) {
     return (fieldValue) {
-      if (Validator.email(fieldValue)) {
+      if (Validator.isEmail(fieldValue)) {
+        return null;
+      } else {
+        return message ?? "Email is not correct";
+      }
+    };
+  }
+
+
+  static FormFieldValidator<String> password({String errorMessage, int minLength = 4,
+    int maxLength,
+    bool shouldContainNumber = false,
+    bool shouldContainSpecialChars = false,
+    bool shouldContainCapitalLetter = false,
+    Function reason,
+  }) {
+    return (fieldValue) {
+      if (Validator.isPassword(fieldValue)) {
         return null;
       } else {
         return errorMessage ?? "Email is not correct";
@@ -132,15 +206,90 @@ class FieldValidators {
     };
   }
 
-  static FormFieldValidator multiple (List<FormFieldValidator<String>> validators) {
+  static FormFieldValidator<String> equalTo(dynamic value, {String message}) {
     return (fieldValue) {
-      for (FormFieldValidator validator in validators) {
-        var outcome = validator(fieldValue);
-        if (outcome != null) return outcome;
+
+      var valueToCompare;
+      if (value is TextEditingController) {
+        valueToCompare = value.text;
+      } else {
+        valueToCompare = value;
       }
-      return null; // all validators were passed
+      if (Validator.isEqualTo(fieldValue, "")) {
+        return null;
+      } else {
+        return message ?? "Email is not correct";
+      }
     };
   }
+
+
+  static FormFieldValidator<String> number({bool noSymbols = true, String message}) {
+    return (fieldValue) {
+      if (Validator.isNumber(fieldValue, noSymbols: noSymbols)) {
+        return null;
+      } else {
+        return message ?? "Field must be numbers only";
+      }
+    };
+  }
+
+  static FormFieldValidator<String> alphaNumeric({String message}) {
+    return (fieldValue) {
+      if (Validator.isAlphaNumeric(fieldValue)) {
+        return null;
+      } else {
+        return message ?? "Field must contain both alphabets and numbers";
+      }
+    };
+  }
+
+
+  static FormFieldValidator<String> minLength(int minLength, {String message}) {
+    return (fieldValue) {
+      if (Validator.minLength(fieldValue, minLength)) {
+        return null;
+      } else {
+        return message ?? "Field must be of minimum length of $minLength";
+      }
+    };
+  }
+
+
+  static FormFieldValidator<String> maxLength(int maxLength, {String message}) {
+    return (fieldValue) {
+      if (Validator.maxLength(fieldValue, maxLength)) {
+        return null;
+      } else {
+        return message ?? "Field must be of maximum length of $maxLength";
+      }
+    };
+  }
+
+  static FormFieldValidator<String> maxValue({double maxValue, String message}) {
+    return (fieldValue) {
+      if (fieldValue.trim().isEmpty) return message ?? "Field must be lesser than $maxValue";
+      double dFieldValue = double.parse(fieldValue.replaceAll(" ", "").replaceAll(",", "").replaceAll(".", ""));
+      if (Validator.maxValue(dFieldValue, maxValue)) {
+        return null;
+      } else {
+        return message ?? "Field must be lesser than $maxValue";
+      }
+    };
+  }
+
+  static FormFieldValidator<String> minValue({double minValue, String message}) {
+    return (fieldValue) {
+      if (fieldValue.trim().isEmpty) return message ?? "Field must be greater than $maxValue";
+      double dFieldValue = double.parse(fieldValue.replaceAll(" ", "").replaceAll(",", "").replaceAll(".", ""));
+      if (Validator.minValue(dFieldValue, minValue)) {
+        return null;
+      } else {
+        return message ?? "Field must be greater than $maxValue";
+      }
+    };
+  }
+
 
   static FormFieldValidator<String> regExp(
       RegExp pattern, [String errorMessage]) {
@@ -153,5 +302,17 @@ class FieldValidators {
         return errorMessage ?? "Do not match the required pattern rules";
     };
   }
+
+
+  static FormFieldValidator multiple (List<FormFieldValidator<String>> validators) {
+    return (fieldValue) {
+      for (FormFieldValidator validator in validators) {
+        var outcome = validator(fieldValue);
+        if (outcome != null) return outcome;
+      }
+      return null; // all validators were passed
+    };
+  }
+
 
 }
